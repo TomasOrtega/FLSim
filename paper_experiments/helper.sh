e82a09ed76d588751a13fd5621896aa165ae1b45
Tomas Ortega <tomas.o.sc@gmail.com>
2023-01-25 09:37:23 -0800

    Add files via upload

---
+++ paper_experiments/helper.sh
@@ -0,0 +1,6 @@
+python3 celeba_example.py --config-file=configs/celeba_config_4bits.json > 4bits1.txt
+python3 celeba_example.py --config-file=configs/celeba_config_4bits.json > 4bits2.txt
+python3 celeba_example.py --config-file=configs/celeba_config_4bits.json > 4bits3.txt
+python3 celeba_example.py --config-file=configs/celeba_config_2bits.json > 2bits1.txt
+python3 celeba_example.py --config-file=configs/celeba_config_2bits.json > 2bits2.txt
+python3 celeba_example.py --config-file=configs/celeba_config_2bits.json > 2bits3.txt
