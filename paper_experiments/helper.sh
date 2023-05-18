ca3f6be362e45d7eabb37de1df4736302240c363
Tomas Ortega <tomas.o.sc@gmail.com>
2023-05-18 10:06:41 -0700

    Recover randk model size

--- paper_experiments/helper.sh
+++ paper_experiments/helper.sh
@@ -1,3 +1,1 @@
-python3 celeba_example.py --config-file=configs/celeba_config_staleness.json > staleness1.txt
-python3 celeba_example.py --config-file=configs/celeba_config_staleness.json > staleness2.txt
-python3 celeba_example.py --config-file=configs/celeba_config_staleness.json > staleness3.txt
+python3 celeba_example.py --config-file=configs/celeba_config_randk.json
